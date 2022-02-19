7c4e68b011934c4fc5ef93bda6244117e24416f6
Oluwasheeun <oluwasheeun@gmail.com>
2022-02-19 02:13:20 -0500

    FeedbackList component & global state

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,21 +1,8 @@
-import { useState } from 'react'
-
-function FeedbackItem() {
-	const [ ratings, setRatings ] = useState(12)
-	const [ text, setText ] = useState('This is my Feedback')
-
-	const handleClick = () => {
-		setRatings((prev) => {
-			console.log(prev)
-			return prev + 1
-		})
-	}
-
+function FeedbackItem({ item }) {
 	return (
 		<div className='card'>
-			<div className='num-display'>{ratings}</div>
-			<div className='text-display'>{text}</div>
-			<button onClick={handleClick}>Click</button>
+			<div className='num-display'>{item.rating}</div>
+			<div className='text-display'>{item.text}</div>
 		</div>
 	)
 }
