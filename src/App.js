674751d79fb6bcaa3cd5791968eb8de8101ba4a8
Oluwasheeun <oluwasheeun@gmail.com>
2022-02-19 00:26:55 -0500

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const title = 'Blog Post'
+  const body = 'These are the posts'
+  const comments = [
+    {
+      id:1, text: 'comment one'
+    },
+    {
+      id:2, text: 'comment two'
+    },
+    {
+      id:3, text: 'comment three'
+    }
+  ]
+	return (
+		<>
+			<h1>{title.toUpperCase()}</h1>
+			<p>{body}</p>
+
+      <div className="comments">
+        <h3>Commnts ({comments.length})</h3>
+        <ul>
+        {comments.map(comment => <li key={comment.id}>{comment.text.toUpperCase()}</li>)}
+        </ul>
+      </div>
+		</>
+	)
 }
 
-export default App;
+export default App
