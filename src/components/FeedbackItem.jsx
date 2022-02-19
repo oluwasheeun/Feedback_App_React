75851d22d1070d01d27da94ff6d613bb0e36baaf
Oluwasheeun <oluwasheeun@gmail.com>
2022-02-19 01:54:12 -0500

    Test State and useState

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react'
+
 function FeedbackItem() {
+	const [ ratings, setRatings ] = useState(12)
+	const [ text, setText ] = useState('This is my Feedback')
+
+	const handleClick = () => {
+		setRatings((prev) => {
+			console.log(prev)
+			return prev + 1
+		})
+	}
+
 	return (
 		<div className='card'>
-			<div className='num-display'>100</div>
-			<div className='text-display'>Ex Feedback</div>
+			<div className='num-display'>{ratings}</div>
+			<div className='text-display'>{text}</div>
+			<button onClick={handleClick}>Click</button>
 		</div>
 	)
 }
