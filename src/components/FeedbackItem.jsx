46b72aa759f608b513958c2bdf84105e1fe3791b
Oluwasheeun <oluwasheeun@gmail.com>
2022-02-26 00:00:07 -0500

    Feedback edit function

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,20 @@
 import { useContext } from 'react'
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackItem({ item }) {
-	const { deleteFeedback } = useContext(FeedbackContext)
+	const { deleteFeedback, editfeedback } = useContext(FeedbackContext)
 
 	return (
 		<Card reverse={true}>
 			<div className='num-display'>{item.rating}</div>
 			<button onClick={() => deleteFeedback(item.id)} className='close'>
 				<FaTimes color='black' />
+			</button>
+			<button onClick={() => editfeedback(item)} className='edit'>
+				<FaEdit />
 			</button>
 			<div className='text-display'>{item.text}</div>
 		</Card>
--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -6,19 +6,33 @@ const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
 	const [ feedback, setFeedback ] = useState(FeedbackData)
+	const [ feedbackEdit, setFeedbackEdit ] = useState({ item: {}, edit: false })
 
+	// Delete Feedback
 	const deleteFeedback = (id) => {
 		if (window.confirm('Are you sure you want to delete?')) {
 			setFeedback(feedback.filter((item) => item.id !== id))
 		}
 	}
 
+	// Add Feedback
 	const addFeedback = (newFeedback) => {
 		newFeedback.id = uuidv4()
 		setFeedback([ newFeedback, ...feedback ])
 	}
+
+	// Edit feedback
+	const editFeedback = (item) => {
+		setFeedbackEdit({
+			item,
+			edit: true
+		})
+	}
+
 	return (
-		<FeedbackContext.Provider value={{ feedback, addFeedback, deleteFeedback }}>{children}</FeedbackContext.Provider>
+		<FeedbackContext.Provider value={{ feedback, addFeedback, deleteFeedback, editFeedback }}>
+			{children}
+		</FeedbackContext.Provider>
 	)
 }
 
