85e7e130d9c851fcf507540686fdc5b0d5e7a866
Oluwasheeun <oluwasheeun@gmail.com>
2022-02-27 22:21:33 -0500

    Fetch data from backend json-server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,12 +1,23 @@
 import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
-import FeedbackData from '../data/FeedbackData'
+import { createContext, useState, useEffect } from 'react'
+import { dblClick } from '@testing-library/user-event/dist/click'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-	const [ feedback, setFeedback ] = useState(FeedbackData)
+	const [ feedback, setFeedback ] = useState([])
 	const [ feedbackEdit, setFeedbackEdit ] = useState({ item: {}, edit: false })
+
+	useEffect(() => {
+		fetchFeedback()
+	}, [])
+
+	// Fetch feedback data from db.json
+	const fetchFeedback = async () => {
+		const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+		const data = await response.json()
+		setFeedback(data)
+	}
 
 	// Delete Feedback
 	const deleteFeedback = (id) => {
